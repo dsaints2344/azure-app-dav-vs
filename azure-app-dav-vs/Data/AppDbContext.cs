6d907ab75eb15e5bcc903886fe946fc33f7a90ed
David De Los Santos <ddelossantos@neural.com.do>
2023-10-23 22:45:31 -0400

    Added EF Core support

---
+++ azure-app-dav-vs/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace azure_app_dav_vs.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> persons { get; set; }
+    }
+}
