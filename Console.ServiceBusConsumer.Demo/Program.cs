574ad9ae6fb138f68e0cd6dfc7da4c1a1ed34fef
David De Los Santos <ddelossantos@neural.com.do>
2023-11-28 15:16:52 -0400

    Adding service consumer demo project

---
+++ Console.ServiceBusConsumer.Demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://course.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=UaY1JOxrY9snTL/D0EhScYUMTuizxJ5B7+ASbAFn9JA=";
+const string queueName = "azure-course-queue-1";
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine(body);
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs)
+{
+    string error = processErrorEventArgs.Exception.ToString();
+    Console.WriteLine(error);
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any to stop processing.");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped recieving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+    throw;
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
